34e92a115e1a499a80cd160013ac19e47af7f111
Tawmie02 <137549834+Tawmie02@users.noreply.github.com>
2025-05-04 16:58:25 +0300

    initial commit

---
+++ hello_cargo/target/debug/deps/hello_cargo-d6d08006b580d179.d
@@ -0,0 +1,5 @@
+C:\Users\ADMIN\projects\hello_cargo\target\debug\deps\libhello_cargo-d6d08006b580d179.rmeta: src\main.rs
+
+C:\Users\ADMIN\projects\hello_cargo\target\debug\deps\hello_cargo-d6d08006b580d179.d: src\main.rs
+
+src\main.rs:
